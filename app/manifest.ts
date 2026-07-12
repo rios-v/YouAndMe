43faaa9e235640a6468ff351be95628f780e9271
RIOSSSSS <leo.santos23032000@gmail.com>
2026-07-12 01:12:10 -0300

    mudança no nome do app

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,8 +2,8 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "NósDois",
-    short_name: "NósDois",
+    name: "Nós Dois",
+    short_name: "Nós Dois",
     description: "Nosso cantinho especial",
     start_url: "/",
     display: "standalone",
